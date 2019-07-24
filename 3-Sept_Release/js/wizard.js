3147f11019eef2fd0dc27113b1ef376526a84812
stephfff <freund.steph@gmail.com>
2019-07-24 11:12:44 -0500

    Dispense HIstory Button Modal

--- 3-Sept_Release/js/wizard.js
+++ 3-Sept_Release/js/wizard.js
@@ -2,3 +2,13 @@
 function printWizard() {
   window.print();
 }
+
+$(document).ready(function() {
+	$('.accordion').find('.accordion-toggle').click(function() {
+		$(this).next().slideToggle('600');
+		$(".accordion-content").not($(this).next()).slideUp('600');
+	});
+	$('.accordion-toggle').on('click', function() {
+		$(this).toggleClass('active').siblings().removeClass('active');
+	});
+});

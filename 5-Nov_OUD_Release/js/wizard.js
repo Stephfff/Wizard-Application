20b449218d356deb253bb9ae39023d91dbcab973
Stephanie Freund <you@example.com>
2019-11-11 12:53:22 -0600

    Initial Commit of OUD code

---
+++ 5-Nov_OUD_Release/js/wizard.js
@@ -0,0 +1,14 @@
+// Print Function for Print Button //
+function printWizard() {
+  window.print();
+}
+
+$(document).ready(function() {
+	$('.accordion').find('.accordion-toggle').click(function() {
+		$(this).next().slideToggle('600');
+		$(".accordion-content").not($(this).next()).slideUp('600');
+	});
+	$('.accordion-toggle').on('click', function() {
+		$(this).toggleClass('active').siblings().removeClass('active');
+	});
+});

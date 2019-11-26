fd29f58fe0f3d435ebaaf9fb321fcbc764b4cf6a
Stephanie Freund <you@example.com>
2019-11-26 10:49:32 -0600

    Updates

--- 5-Nov_OUD_Release/js/wizard.js
+++ 5-Nov_OUD_Release/js/wizard.js
@@ -12,3 +12,12 @@ $(document).ready(function() {
 		$(this).toggleClass('active').siblings().removeClass('active');
 	});
 });
+
+function toggleIcon(e) {
+    $(e.target)
+        .prev('.panel-heading')
+        .find(".more-less")
+        .toggleClass('glyphicon-plus glyphicon-minus');
+}
+$('.panel-group').on('hidden.bs.collapse', toggleIcon);
+$('.panel-group').on('shown.bs.collapse', toggleIcon);
